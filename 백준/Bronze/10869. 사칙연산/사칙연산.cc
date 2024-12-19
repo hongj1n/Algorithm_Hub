81a8d303812dfa86a880b88e2e469ebef01178ef
hongj1n <wlsnqkr0607@gmail.com>
2024-12-19 17:15:55 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A;
+    int B;
+    cin >> A;
+    cin >> B;
+
+    cout << A + B << endl;
+    cout << A - B << endl;
+    cout << A * B << endl;
+    cout << A / B << endl;
+    cout << A % B << endl;
+    return 0;
+}
