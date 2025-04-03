03b2d37fdf6fc5d4b5644989a620e3c84638e508
hongj1n <wlsnqkr0607@gmail.com>
2025-04-03 15:22:56 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    for (int i = 1; i <= x; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << endl;
+    }
+    return 0;
+}
